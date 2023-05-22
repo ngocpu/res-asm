3dc0be199033fd27a523c587cc9dd71a445965c8
ngocpu <116057721+ngocpu@users.noreply.github.com>
2023-05-22 09:25:26 +0700

    Update restaurant.asm

--- restaurant.asm
+++ restaurant.asm
@@ -2,35 +2,23 @@
 .stack 100h
 .data
 
-    m0 dw "         !!!!!!Welcome in our project!!!!!!$"
-    m1 dw 10,13,10,13, "Which menu do you want ??please select:$"
-    m2 dw 10,13,10,13, "1.Rice 100/- 2.Vegetable 50/- 3.Soup 20/- $"
-    m3 dw 10,13,10,13, "Select the menu number:$"
-    m8 dw 10,13,10,13, "SORRY!!!There is no more than 3 item,if u want,u can add one$"
-    m9 dw 10,13,10,13, "Enter Food name:$"
-    m10 dw 10,13,10,13,"          Price:$" 
-    m4 dw 10,13,10,13, "To add press 1 or press 2 to get back menu :$"
+    m0 dw "         !!!!!Welcome in our project!!!!!$"
+    m2 dw 10,13,10,13, "Menu:1.Product1  100/- 2.Product2  50/- 3.Product3  20 $"
+    m3 dw 10,13,10,13, "Please select the menu number:$"
+    m8 dw 10,13,10,13, "SORRY!!!There is no more than 3 item!$" 
+    m4 dw 10,13,10,13, "Please go to back menu!$"
     m5 dw 10,13,10,13, "Enter quantity:$" 
     m6 dw 10,13,10,13, "Total price: $"
-    m7 dw 10,13,10,13, "      *****THANK YOU*****$"
-    m11 dw "4.$"  
-    m12 dw "/-$" 
-    m13 dw 10,13,10,13, " Re-odrer : Press <1>$",
-    m14 dw 10,13,10,13, " Exit : Press Any key$"
+    m7 dw 10,13,10,13, "*****THANK YOU*****$"
+    m13 dw 10,13,10,13, "Next-odrer : Press <1>$",
+    m14 dw 10,13,10,13, "Exit : Press Any key$"
     m15 dw 10,13,10,13, "Final Total Price:$"
     
     tong dw 0 
-    q dw 0    
-    r dw 0
-    v db 0
-    s dw 0
-    rprice dw 100
-    vprice dw 50
-    sprice dw 20
-    nprice dw 0
-    
-    var1 db 100 dup('$')
-    
+    1price dw 100
+    2price dw 50
+    3price dw 20
+      
 .code
     main proc
         
@@ -41,163 +29,82 @@
         Lea dx,m0
         int 21h  
         
-        start:
-        cmp v,0
-        jg start1
-        
-        mov ah,9
-        Lea dx,m1
-        int 21h
-        
         menu:
-        
+        ;in ra chuoi m2
         mov ah,9
         Lea dx,m2
         int 21h
         
+        ;in ra chuoi m3
         mov ah,9
         Lea dx,m3
         int 21h
         
+        ;nhap so thu tu cua mon an muon chon
         mov ah,1
         int 21h        
         
+        ;so sanh gia tri da nhap voi stt nhung mon an co trong danh sach
         cmp al,31h
-        je rice_
+        je product1
         cmp al,32h
-        je veg_
+        je product2
         cmp al,33h
-        je soup_
-         
+        je product3
         
-        
-        menuadd:
-        inc v
-        
+                 
+        re_select:
+        ;in chuoi m8
         mov ah,9
         Lea dx,m8
         int 21h 
         
+        ;in chuoi m4
         mov ah,9
         Lea dx,m4
         int 21h
         
-        mov ah,1
-        int 21h
-        cmp al,32h
-        je menu
-                
-        mov ah,9
-        Lea dx,m9
-        int 21h
+        ;nhay den ham menu
+        jmp menu  
         
-        mov si,offset var1
-        
-        l1:
-        mov ah,1
-        int 21h
-        cmp al,13
-        je print
-        mov [si],al
-        inc si
-        jmp l1
-        
-        print: 
-        
-        call price
-        
-        start1:
-        
-        mov ah,9
-        Lea dx,m2
-        int 21h
-        
-        mov ah,9
-        Lea dx,m11
-        int 21h  
-        
-        mov dx,offset var1
-        mov ah,9
-        int 21h
-        
-        mov ah,2
-        mov dl,' '
-        int 21h 
-        
-        xor ax,ax   
-        mov ax,nprice
-        call outdec
-        
-        mov ah,9
-        Lea dx,m12
-        int 21h 
-        
-        
-        
-        mov ah,9
-        Lea dx,m3
-        int 21h
-        
-        mov ah,1
-        int 21h        
-        
-        cmp al,31h
-        je rice_
-        cmp al,32h
-        je veg_
-        cmp al,33h
-        je soup_
-        
-        newmenu_:
-        
-            mov ah,9
-            Lea dx,m5
-            int 21h
-            
-            xor ax,ax            
-            
-            call indec
-    
-            mul nprice
-            
-            mov bx,ax
-            
-            jmp totalprice
-         
-        
-        veg_:
-        
-            mov ah,9
-            Lea dx,m5
-            int 21h
-            
-            xor ax,ax            
-            
-            call indec
-    
-            mul vprice
-            
-            mov bx,ax
-            
-            jmp totalprice
-        
-        rice_:
-        
+        product1:
+            ;in chuoi m5
             mov ah,9
             Lea dx,m5
             int 21h 
             
+            ;xoa noi dung thanh ghi ax
             xor ax,ax
             
+            ;goi indec
             call indec
             
-            mul rprice 
+            ;tinh gia tien
+            mul 1price 
+            
+            ;chuyen gia tien vao bx
+            mov bx,ax
+            
+            ;nhay den totalprice
+            jmp totalprice
+                
+        product2:
+        
+            mov ah,9
+            Lea dx,m5
+            int 21h
+            
+            xor ax,ax            
+            
+            call indec
+    
+            mul 2price
             
             mov bx,ax
             
             jmp totalprice 
             
-        soup_:    
+        product3:    
             mov ah,9
             Lea dx,m5
             int 21h 
@@ -206,86 +113,58 @@
             
             call indec
            
-            mul sprice
+            mul 3price
             
             mov bx,ax
             
             jmp totalprice 
-            
-        price:
-        
-            mov ah,9
-            Lea dx,m10
-            int 21h  
-            
-            mov ax,0
-             mov bx,0
-              mov cx,0
-               mov dx,0
-            
-            input:
-                and ax,000Fh
-                push ax
-                mov ax,10
-                mul bx
-                mov bx,ax
-                pop ax
-                add bx,ax
                 
-                mov ah,1
-                int 21h
-                
-                cmp al,0Dh
-                jne input
-                
-            add nprice,bx
-            ret
-               
-            
-            
         totalprice:
-         
+            ;in chuoi m6
             mov ah,9
             Lea dx,m6
             int 21h 
             
-            xor ax,ax
+            xor ax,ax ;xoa noi dung thanh ghi ax
+            add tong,bx ;tinh tong cua tong + bx luu vao tong
+            mov ax,bx ;chuyen gia tri bx sang ax 
+            call outdec ;goi outdec
             
-            add tong,bx
-            mov ax,bx
-            call outdec
-            
+            ;in chuoi m13
             mov ah,9
             Lea dx,m13
             int 21h
             
+            ;in chuoi m14
             mov ah,9
             Lea dx,m14
             int 21h
             
+            ;nhap so de tiep tuc hoac ket thuc chuong trinh
             mov ah,1
             int 21h
             
+            ;so sanh 
             cmp al,31h
-            je start
+            je menu
             
+            ;in chuoi m15
             mov ah,9
             lea dx,m15
             int 21h
-            mov ax,tong
-            call outdec
-            
+          
+            mov ax,tong ;chuyen gia tri cua tong vao ax            
+            call outdec ;goi outdec
+                        
+            ;in chuoi m7
             mov ah,9
             Lea dx,m7
             int 21h
-        
-        
+            
         mov ah,4ch
         int 21h
         
         main endp
-    include indec.txt
-    include outde.txt
+    include indec.txt  
+    include outdec.txt 
     end main
-        
-    
